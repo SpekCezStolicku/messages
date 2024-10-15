172ea223c8c1c7864322ee6a7607d3edfdf368ff
Speky <martin.mozucha@gmail.com>
2024-10-16 00:11:51 +0200

    add DI

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public MessagesService: MessagesService) {}
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.MessagesService.findAll();
+  }
 
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.MessagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage() {}
+  async getMessage(@Param('id') id: string) {
+    const message = await this.MessagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message;
+  }
 }
